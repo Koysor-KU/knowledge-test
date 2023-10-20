01ea9714a1dba5b10677e182e3032721c9308c24
Koysor-KU <147259797+Koysor-KU@users.noreply.github.com>
2023-10-20 14:46:48 +0100

    Update js file.js

--- convert your age from “human years” to “dog years”/js file.js
+++ convert your age from “human years” to “dog years”/js file.js
@@ -1,1 +1,25 @@
 
+let humanAge= 18; //my age veriable
+let earlyYears = 2; //earlyYear variable
+
+earlyYears *=10.5;
+let laterYears=humanAge-2;//removed early years as early years mutiply diffrently. 
+laterYears*= 4;
+ // we took the later years which gets mutiplyed by 4 . 16*4=
+console.log(`Human's Name earlyYears in dog age ` +earlyYears);
+console.log(`Human's Name laterYears in dog years ` + laterYears);
+
+humanInDogYears = earlyYears+laterYears;
+//added early age which is *10.5 +later year which is *4.
+console.log( `Human's Name full age doog year` +humanInDogYears);
+
+ let humanName=`Human's Name`.toLocaleLowerCase();
+ //I put the human's name string inside human name variable and put it in lower case method.
+
+console.log(`My name is ${humanName}. I am ${humanAge} 
+years old in human years which is ${humanInDogYears} year old in dog years. `);
+//took the value and put them togather in a sentence. 
+
+
+
+
