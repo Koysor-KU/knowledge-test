91e0303171964f6fa6d7f933597558e409775ad9
Koysor Uddin <Koysor.Uddin@UnitedResponse.org.uk>
2023-10-20 13:30:57 +0100

    Update js script.js
    recreated the function with two variable result. newton and Fahrenheit.

--- convert Kelvin to Celsius, then to Fahrenheit/js script.js
+++ convert Kelvin to Celsius, then to Fahrenheit/js script.js
@@ -37,3 +37,39 @@ In the given code, the line is added to provide a way to retrieve the converted 
 let kelvinTemp = 280;
 let result = convertTemperature(kelvinTemp);
 console.log(`The temperature is ${result} degrees Fahrenheit.`);
+
+
+
+
+//part two. 
+//I have added new variale (newton). The function provide both fahrenheit and newton. 
+
+//top code provide one value which is why result veriable work but but with two formula, i had to redo the function. 
+
+
+function convertTemperature(kelvinTemp)/* create  fuction and add kelvinTemp variable*/
+{ 
+  let celsius = kelvinTemp - 273; /* celsius veriable with formula */
+  let fahrenheit= celsius*(9/5)+32; 
+  fahrenheit= Math.floor(fahrenheit);
+  /* fahrenheit veriable formula and round the number*/
+
+  let newtonResult = celsius * (33/100);
+  newtonResult= Math.floor(newtonResult);
+  /* newton veriable formula and round the number*/
+  
+  return [newtonResult, fahrenheit];
+  /* return the veriables result so that they can be used outside the fuction*/
+  }
+
+  let kelvinTemp = 293;
+  /*  inserted the main kelvin temp value */
+  let [ newtonResult, fahrenheit] = convertTemperature(kelvinTemp);
+/* created new veriables which holds the retun value*/
+
+  console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+  console.log(`The temperature is ${newtonResult} newton.`);
+
+  //result:The temperature is 68 degrees Fahrenheit.
+  //The temperature is 6 newton.
